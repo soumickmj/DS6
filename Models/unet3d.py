71e9c112040e6099a61d976f70ea30b1d5569ffa
chethanMysore <chethanmr.2694@gmail.com>
2022-01-20 21:37:51 +0100

    Replaced Leaky ReLU with PReLU

--- Models/unet3d.py
+++ Models/unet3d.py
@@ -32,11 +32,11 @@ class conv_block(nn.Module):
             nn.Conv3d(in_channels=in_channels, out_channels=out_channels, kernel_size=k_size,
                       stride=stride, padding=padding, bias=bias),
             nn.BatchNorm3d(num_features=out_channels),
-            nn.LeakyReLU(inplace=True),
+            nn.PReLU(init=0.25),
             nn.Conv3d(in_channels=out_channels, out_channels=out_channels, kernel_size=k_size,
                       stride=stride, padding=padding, bias=bias),
             nn.BatchNorm3d(num_features=out_channels),
-            nn.LeakyReLU(inplace=True)
+            nn.PReLU(init=0.25),
         )
 
     def forward(self, x):
@@ -57,7 +57,7 @@ class up_conv(nn.Module):
             nn.Conv3d(in_channels=in_channels, out_channels=out_channels, kernel_size=k_size,
                       stride=stride, padding=padding, bias=bias),
             nn.BatchNorm3d(num_features=out_channels),
-            nn.LeakyReLU(inplace=True))
+            nn.PReLU(init=0.25))
 
     def forward(self, x):
         x = self.up(x)
