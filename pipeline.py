Fixed miploss bug with averaging

pipeline.py
@@ -268,8 +268,8 @@ class Pipeline:
                                 op_mip = torch.amax(op, -1)
                                 mip_loss_patch += loss_ratios[level] * self.focalTverskyLoss(op_mip,
                                                   patches_batch['ground_truth_mip_patch'][index].float().cuda())
-                            mip_loss += mip_loss_patch / len(op) if not torch.isnan(mip_loss_patch / len(op)) \
-                                else torch.tensor(0).float().cuda()
+                            if not torch.any(torch.isnan(mip_loss_patch)):
+                                mip_loss += mip_loss_patch / len(output)
                             # mip_loss += loss_ratios[level] * self.mip_loss(output, patches_batch, self.pre_loaded_train_lbl_data, self.focalTverskyLoss, self.patch_size)
 
                             level += 1