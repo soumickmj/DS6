d44d1e48d2086e59abee797cbd2e537e2273d8d8
Soumick <soumick@live.com>
2022-09-23 21:12:26 +0200

    view to reshape for new dist loss

--- main_executor3DFID.py
+++ main_executor3DFID.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
                         default=True, action=argparse.BooleanOptionalAction,
                         help="To compute loss by comparing distributions of output and GT (for ProbUNet)")
     parser.add_argument('--distloss_mode',
-                        default=2, type=int,
+                        default=0, type=int,
                         help="0: Pure FID for distloss (repeats the input to make 3 channels as pretrained on RGB imagenet) \n"
                              "1: For Fréchet ResNeXt Distance (trained on single-channel MRIs) \n"
                              "2: GeomLoss Sinkhorn (Default cost function) \n"
@@ -120,7 +120,7 @@ if __name__ == '__main__':
 
     parser.add_argument("--batch_size",
                         type=int,
-                        default=4,
+                        default=50,
                         help="Batch size for training")
     parser.add_argument("--batch_size_fidloss",
                         type=int,
@@ -139,23 +139,23 @@ if __name__ == '__main__':
                         default=64,
                         help="Patch size of the input volume")
     parser.add_argument("--slice2D_shape",
-                        default="480,640",
+                        default="",
                         help="For 2D models, set it to the desired shape. Or blank")
     parser.add_argument("--stride_depth",
                         type=int,
-                        default=1,
+                        default=16,
                         help="Strides for dividing the input volume into patches in depth dimension (To be used during validation and inference)")
     parser.add_argument("--stride_width",
                         type=int,
-                        default=640,
+                        default=32,
                         help="Strides for dividing the input volume into patches in width dimension (To be used during validation and inference)")
     parser.add_argument("--stride_length",
                         type=int,
-                        default=480,
+                        default=32,
                         help="Strides for dividing the input volume into patches in length dimension (To be used during validation and inference)")
     parser.add_argument("--samples_per_epoch",
                         type=int,
-                        default=1500,
+                        default=4000,
                         help="Number of samples per epoch")
     parser.add_argument("--num_worker",
                         type=int,
