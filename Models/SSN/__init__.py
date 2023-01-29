cfe29cf5ac1a2fa94855821ecae3ff1a6f70619e
Administrator <admin+iks@iks.cs.uni-magdeburg.de>
2023-01-29 13:49:29 +0000

    Fixed imports for SSN

---
+++ Models/SSN/__init__.py
@@ -0,0 +1,2 @@
+#The code of this package (Models.SSN) is based on the code from the following repository: https://github.com/biomedia-mira/stochastic_segmentation_networks
+#based on the following paper: https://arxiv.org/abs/2006.06015 ["Stochastic Segmentation Networks: Modelling Spatially Correlated Aleatoric Uncertainty" by Miguel Monteiro et al.]
