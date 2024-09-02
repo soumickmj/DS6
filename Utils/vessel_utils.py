d55f1e9624ad623a69498644af07e94ff8c00355
Soumick Chatterjee <soumick@live.com>
2024-09-02 23:58:03 +0200

    will able to load checkpoints without optimiser state (e.g. downloaded from huggingface)

--- Utils/vessel_utils.py
+++ Utils/vessel_utils.py
@@ -70,7 +70,8 @@ def load_model(model, optimizer, CHECKPOINT_PATH, batch_index='best', filename='
     print('Loading model...')
     checkpoint = torch.load(os.path.join(CHECKPOINT_PATH, filename + str(batch_index) + '.pth'))
     model.load_state_dict(checkpoint['state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer'])
+    if 'optimizer' in checkpoint:
+        optimizer.load_state_dict(checkpoint['optimizer'])
     model.eval()
     return model, optimizer
 
